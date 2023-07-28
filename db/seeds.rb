cdfca0853acd4b7480c044285c06027e5d452e34
naanmohammed <naanahmed24@gmail.com>
2023-07-28 20:18:47 +0300

    Update migrations and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,18 @@ first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0Bx
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
 
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-first_post = Post.create(author: first_user, title: 'Second', text: 'This is my second post')
-first_post = Post.create(author: first_user, title: 'Third', text: 'This is my third post')
-first_post = Post.create(author: first_user, title: 'Fourth', text: 'This is my fourth post')
+second_post = Post.create(author: first_user, title: 'Second', text: 'This is my second post')
+third_post = Post.create(author: second_user, title: 'Third', text: 'This is my third post')
+fourth_post = Post.create(author: second_user, title: 'Fourth', text: 'This is my fourth post')
 
 
 # Create comments
 Comment.create(post: first_post, author: second_user, text: 'Hi Naan!')
 Comment.create(post: first_post, author: second_user, text: 'Hi Leul!')
-Comment.create(post: first_post, author: second_user, text: 'Hi Melaku!')
-Comment.create(post: first_post, author: second_user, text: 'Hi Samri!')
-Comment.create(post: first_post, author: second_user, text: 'Hi Yohannes!')
-Comment.create(post: first_post, author: second_user, text: 'Hi Yordi!')
+Comment.create(post: second_post, author: second_user, text: 'Hi Melaku!')
+Comment.create(post: second_post, author: first_user, text: 'Hi Samri!')
+Comment.create(post: third_post, author: first_user, text: 'Hi Yohannes!')
+Comment.create(post: fourth_post, author: first_user, text: 'Hi Yordi!')
 
 # Find, update, and delete entities
 user = User.find_by(name: 'Tom')
