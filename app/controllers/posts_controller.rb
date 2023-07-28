908e1639587dac3961baa5ed64071cf3bd5211c5
naanmohammed <naanahmed24@gmail.com>
2023-07-28 21:34:01 +0300

    Add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,32 @@
 class PostsController < ApplicationController
   def index
-    # Logic for index action
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
-    # Logic for show action
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id), notice: 'Post was successfully created.'
+    else
+      flash.now[:alert] = 'An error has occurred while creating the post.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 
 end
