303515b85a7468f8ca8d8122a6aa4a9e74b4f555
naanmohammed <naanahmed24@gmail.com>
2023-08-03 22:55:48 +0300

    Add ability model

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    #
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post, author_id: user.id
+      can :create, Comment, author_id: user.id
+      can :create, Like
+      can :destroy, Post do |post|
+        post.author_id == user.id
+      end
+      can :destroy, Comment do |comment|
+        comment.author_id == user.id
+      end
+    end
+  end
+end
