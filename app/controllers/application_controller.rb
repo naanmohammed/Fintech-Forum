84cc41c97cdfe65c1edf5d6f29155b992e47aebd
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-08-05 17:04:03 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!, unless: :api_request?
+  before_action :update_allowed_parameters, if: :devise_controller?, unless: :api_request?
+  protect_from_forgery with: :exception, unless: :api_request?
 
   protected
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :email, :password, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :email, :password, :photo])
+  end
+
+  def api_request?
+    controller_path.starts_with?("api/")
   end
 end
