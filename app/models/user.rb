5e14fa008b2edb9aa9cc5087fc8e58aec8066045
naanmohammed <naanahmed24@gmail.com>
2023-08-05 16:37:19 +0300

    Revert "Update models and controllers for api"
    This reverts commit e9a44612b89358275cdaab43bebf8932896240ef.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,6 @@ class User < ApplicationRecord
   validates :name, presence: true, allow_blank: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
-  before_save :generate_api_token
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
@@ -28,12 +26,5 @@ class User < ApplicationRecord
 
   def update_comments_counter
     update(comments_counter: comments.count)
-  end
-
-
-  private
-
-  def generate_api_token
-    self.api_token = SecureRandom.hex(16)
   end
 end
